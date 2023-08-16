27a6fb417dae53376f78c0c91b6cdb52c727c58b
Mario Tabares <alejndro.tabares@udea.edu.co>
2023-08-16 17:13:51 -0500

    CorrecionDeComposicionDeCarpetas

--- scr/index.js
+++
@@ -1,3 +0,0 @@
-var nombre = 'Mario';
-var edad = 29;
-console.log("Hello World, mi nombre es :" + nombre + "y tengo" + edad);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 let nombre = "Mario";
-
-
 console.log("Hello World, mi nombre es :" +nombre+"y tengo" );
